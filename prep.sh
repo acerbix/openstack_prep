32205e9cac51c8722e106fe7b82da8f5825b0b8a
root <root@base.openstack.subhajit.net>
2014-12-29 20:09:27 -0700

    a

--- prep.sh
+++ prep.sh
@@ -8,7 +8,7 @@ echo "Setting up subscription-manager..."
 subscription-manager attach --pool=8a85f9864a7bffa2014a7e1e28bc39d1
 
 echo "Installing base packages..."
-yum install net-tools mlocate vim-enhanced vim-filesystem telnet psmisc bind-utils git
+yum install net-tools mlocate vim-enhanced vim-filesystem telnet psmisc bind-utils git screen ntp
 
 echo "Installing vimrc..."
 cp vimrc ~/.vimrc
@@ -19,6 +19,9 @@ cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo "Copying the route service..."
 cp route.service /usr/lib/systemd/system/route.service
+
+echo "Copying hosts..."
+cp hosts /etc/hosts
 
 echo <<EoT
 File copying done. 
